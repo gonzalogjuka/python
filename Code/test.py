b601f686def4f62a0155a85c59dd8f1f82d74e07
Gonza <gonzalo.gjuka@hotmail.com>
2022-12-23 21:50:00 -0300

    code

---
+++ Code/test.py
@@ -0,0 +1,21 @@
+"""Print('Hello '+'World') // las comillas triples son comentarios
+string1 = 'falo'
+string2 = 'pero'
+string3 = string1+string2
+a=20
+print (a)"""
+
+
+"""number_one = input('Ingresa un numero')
+number_two = input('Ingresa un numero')
+number_one = int(number_one)
+number_two = int(number_two)
+print('Calculando ...')
+result = number_one + number_two
+""""para concatenar un string con un int o otro tipo de dato, tenemos que convertirlo a string de la siguiente manera"""
+""""print ('El resultado es: ' + str(result))"""
+
+a = 3
+b = float (a)
+print(b*a)
+
