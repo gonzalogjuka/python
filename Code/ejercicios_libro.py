75cf4526ec1f068929b0139150cd9316a4952ac2
Gonza <gonzalo.gjuka@hotmail.com>
2023-01-09 17:03:50 -0300

    'ejercicio 2 libro'

--- Code/ejercicios_libro.py
+++ Code/ejercicios_libro.py
@@ -2,7 +2,7 @@ ent = input ('Introduzca temperatura')
 try:
     fahr = float(ent)
     cel = (fahr - 32.0) * 5.0 / 9.0
-    cel = round
-    print (cel)
+    redondeo = round (cel)
+    print (redondeo)
 except: 
     print('Ingrese un numero, por favor ')
