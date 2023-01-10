0e9f146435be6a4da1a7acf989d1404ecaeef25a
Gonza <gonzalo.gjuka@hotmail.com>
2023-01-10 17:12:49 -0300

    'ejercico 1 v2.0'

--- Code/ejercicios_libro.py
+++ Code/ejercicios_libro.py
@@ -1,3 +1,4 @@
+""""
 ent = input ('Introduzca temperatura')
 try:
     fahr = float(ent)
@@ -6,3 +7,23 @@ try:
     print (redondeo)
 except: 
     print('Ingrese un numero, por favor ')
+"""
+
+horas= int (input('introduzca horas'))
+tarifa= int (input('introduzca tarifa'))
+
+
+if horas > 40:
+    horas_extras = horas - 40
+    tarifa_extra = tarifa * 1.5
+    calculo_extras = horas_extras * tarifa_extra
+    salario = tarifa * 40
+    nuevo_salario = salario + calculo_extras
+    print(nuevo_salario)
+else:
+    salario_normal= horas * tarifa
+    print(salario_normal)
+    
+
+     
+
