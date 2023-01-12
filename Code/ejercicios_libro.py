90780a05d251377c7e9a6097a18859d350e30993
Gonza <gonzalo.gjuka@hotmail.com>
2023-01-11 21:53:54 -0300

    'Ejercicio 2 y 3 libro OK'

--- Code/ejercicios_libro.py
+++ Code/ejercicios_libro.py
@@ -7,23 +7,66 @@ try:
     print (redondeo)
 except: 
     print('Ingrese un numero, por favor ')
-"""
 
-horas= int (input('introduzca horas'))
-tarifa= int (input('introduzca tarifa'))
 
+horas= input('Introduzca horas ')
+tarifa= input('Introduzca tarifa ') ----> hacemos inputs generales para cualquier informacion
+try:
+    horas = int(horas)  ----------> si usamos try and except, podemos convertir dicha informacion previo a los parametros que vamos a indicar, tambien podemos indicarselo afuera del if y convertir al tipo de dato
+                                    que nesecitemos manipular
+    tarifa = int (tarifa)
+    if horas > 40:
+        horas_extras = horas - 40
+        tarifa_extra = tarifa * 1.5
+        calculo_extras = horas_extras * tarifa_extra
+        salario = tarifa * 40
+        nuevo_salario = salario + calculo_extras
+        print(nuevo_salario)
+    else:
+        salario_normal= horas * tarifa
+        print(salario_normal)
+except:
+        print('Error,por favor coloque un valor numerico.')
+
+
+EJ2:
+
+horas= input('Introduzca horas ')
+tarifa= input('Introduzca tarifa ')
+horas = int(horas)
+tarifa = int(tarifa)
 
 if horas > 40:
-    horas_extras = horas - 40
-    tarifa_extra = tarifa * 1.5
-    calculo_extras = horas_extras * tarifa_extra
-    salario = tarifa * 40
-    nuevo_salario = salario + calculo_extras
-    print(nuevo_salario)
+        horas_extras = horas - 40
+        tarifa_extra = tarifa * 1.5
+        calculo_extras = horas_extras * tarifa_extra
+        salario = tarifa * 40
+        nuevo_salario = salario + calculo_extras
+        print(nuevo_salario)
 else:
-    salario_normal= horas * tarifa
-    print(salario_normal)
+    salario_normal = horas * tarifa
+    salario_str = str(salario_normal)
+    print('Me gustan los billetes')
+    print('Y no olvides que tu salario es de: $' + salario_str + ' pesos puerco espin')
+
+
+puntaje = input('Introduzca puntuacion: ') -------> recordar siempre hacer un input sin tipo de variable y la misma meterla adentro de los condicionales para que recorra las funciones
+                                                    sino intentara convertir el tipo de dato que introduzcamos a el tipo de dato que le indiquemos
+
+try:
+    puntaje = float(puntaje)
+    if puntaje >= 0.9:
+        print('Sobresaliente')
+    elif puntaje >= 0.8:
+        print('Notable')
+    elif puntaje >= 0.7:
+        print('Bien')
+    elif puntaje >= 0.6:
+        print('Suficiente')
+    else:
+        print('Insufuciente')
+except:
+    print('Puntuacion Incorrecta.')
+
     
-
-     
-
+"""
