ec59f465428eb5cc3daa0f1ba924315cc4548865
Gonza <gonzalo.gjuka@hotmail.com>
2023-01-27 02:16:28 -0300

    a

--- Code/CleanLog.py
+++ Code/CleanLog.py
@@ -6,8 +6,9 @@ infile = askopenfilename()
 outfile = asksaveasfilename()
 error = asksaveasfilename()
 
+
 clean_phrases = ["#####","<?xml","npreMemory.c{DEP}","npDrvPublEVT","npreFindClose","SendRecvTCPMsg","npreMsg.c{DEP}@2922","npreControlProcess.c{DEP}@149","npreOSItf.c{DEP}@796","npreOSItf.c{DEP}@254"]
-db = ["ERROR","N E W"]
+db = ["ERROR","N E W "]
 
 with open(infile, "r") as inf, open(outfile, "w") as outf, open(error,"w") as errores:
 
