b02816f3cc74d30fcabf68bf9c18cfbdaa5da1d8
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-02 14:25:16 -0300

    'optimizado clean_log'

--- Code/CleanLog.py
+++ Code/CleanLog.py
@@ -2,31 +2,27 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 # pyright: reportUnboundVariable=false
 
-pregunta = input('Que accion queres tomar?: \n\t1- Resumir Log\n\t2- Busqueda de solo errores\n')
-
-Tk().withdraw() 
-try:
-    if pregunta == '1':
-        infile = askopenfilename()
-        outfile = asksaveasfilename()
-    if pregunta == '2':
-        error = asksaveasfilename()
-        outfile = asksaveasfilename()
-except:
-    print('Por favor ingresar un valor correcto')
-
-
-clean_phrases = ["#####","<?xml","npreMemory.c{DEP}","npDrvPublEVT","npreFindClose","SendRecvTCPMsg","npreMsg.c{DEP}@2922","npreControlProcess.c{DEP}@149","npreOSItf.c{DEP}@796","npreOSItf.c{DEP}@254"]
+clean_phrases = ["DEBUG"]
 db = ["ERROR","N E W "]
 
-with open(infile, "r") as inf, open(outfile, "w") as outf, open(error,"w") as errores:
+Tk().withdraw()
+pregunta = input('Que accion queres tomar?: \n\t1- Resumir Log\n\t2- Busqueda de solo errores\n\t3- Salir\n')
+if pregunta == '3':exit()
+if pregunta == '1':
+    infile = askopenfilename()
+    outfile = asksaveasfilename()
+    with open(infile, "r") as inf, open(outfile, "w") as outf:
+        for line in inf:
+            if any(phrase in line for phrase in clean_phrases):         
+                line = line.rstrip()
+                outf.write(line + '\n')
+elif pregunta == '2':
+    infile = askopenfilename()
+    error = asksaveasfilename()
+    with open(infile, "r") as inf, open(error, "w")as errores:
+        for line in inf:
+            if any(phrase in line for phrase in db):
+                line = line.rstrip()
+                errores.write(line + '\n')
+                
 
-    for line in inf:
-        if not any(phrase in line for phrase in clean_phrases):         
-            line = line.rstrip()
-            outf.write(line + '\n')
-            outf.close()
-        if any(phrase in line for phrase in db):
-            line = line.rstrip()
-            errores.write(line + '\n')
-            errores.close()
