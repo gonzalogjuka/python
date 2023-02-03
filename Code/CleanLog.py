904d951a275aa613e1cdf0509f871885e635008b
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-03 02:18:45 -0300

    'fix issue with search'

--- Code/CleanLog.py
+++ Code/CleanLog.py
@@ -1,25 +1,28 @@
 from tkinter import Tk 
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 # pyright: reportUnboundVariable=false
-# ["#####","<?xml","npreMemory.c{DEP}","npDrvPublEVT","npreFindClose","SendRecvTCPMsg","npreMsg.c{DEP}@2922","npreControlProcess.c{DEP}@149","npreOSItf.c{DEP}@796","npreOSItf.c{DEP}@254"]
-log = ["#####","<?xml","npreMemory.c{DEP}","npDrvPublEVT","npreFindClose","SendRecvTCPMsg","npreMsg.c{DEP}@2922","npreControlProcess.c{DEP}@149","npreOSItf.c{DEP}@796","npreOSItf.c{DEP}@254"]
-errors = ["ERROR","N E W "]
+
+log=["DEBUG","INFO","WARNING"]
+not_log = ["#####","<?xml","npreMemory.c{DEP}","npDrvPublEVT","npreFindClose","SendRecvTCPMsg","npreMsg.c{DEP}@2922","npreControlProcess.c{DEP}@149","npreOSItf.c{DEP}@796","npreOSItf.c{DEP}@254","ERROR","WARNING","N E W"]
+errors = ["ERROR","N E W ","WARNING","FATAL"]
+not_erros= ["ERROR_LOG","Error de comunicacion","Error executing command","npreFindClose","SendRecvTCPMsg resource","InsertResource resource","WARNING_LOG","COD0028","npreMsg2_SendRecvEx","GetResource:","Configuration parameter","ChangeItemPriceBySaleType","PosShowPrice","PosDoTryGrillEnd","cPosCheckPromotedOrder","PosConvertProduct","The Offers Server took more","sharpmessaging.cpp{DEP}@225","npreMemory.c","npDrvPublEVT.c"]
 
 Tk().withdraw()
 pregunta = input('Que accion queres tomar?: \n\t1- Resumir Log\n\t2- Busqueda de solo errores\n\t3- Salir\n')
 if pregunta == '3':exit()
+
 if pregunta == '1':
     infile = askopenfilename()
     outfile = asksaveasfilename()
     with open(infile, "r") as inf, open(outfile, "w") as outf:
         for line in inf:
-            if not any(phrase in line for phrase in log):         
-                f = line.split()
-                del f [0:7]
-                delimited=' '
-                f = delimited.join(f)
-                f = line.rstrip()
-                outf.write(f + '\n')
+            if not any(phrase in line for phrase in not_log): 
+                if any(phrase in line for phrase in log):       
+                    f = line.split()  
+                    del f [0:5]
+                    delimited=' '
+                    b = delimited.join(f)          
+                    outf.write(b + '\n')
 
 elif pregunta == '2':
     infile = askopenfilename()
@@ -27,11 +30,11 @@ elif pregunta == '2':
     with open(infile, "r") as inf, open(error, "w")as errores:
         for line in inf:
             if any(phrase in line for phrase in errors):
-                line = line.rstrip()
-                f = line.split()
-                del f [0:7]
-                delimited=' '
-                b = delimited.join(f)
-                errores.write(b + '\n')
+                    if not any(phrase in line for phrase in not_erros):  
+                        f = line.split()
+                        del f [0:5]
+                        delimited=' '
+                        b = delimited.join(f)
+                        errores.write(b + '\n')
                 
 
