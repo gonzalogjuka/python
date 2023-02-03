62a9ad2570177ce92f27ea014b38c2ee492a3b38
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-03 02:30:58 -0300

    'v2'

--- Code/CleanLog.py
+++ Code/CleanLog.py
@@ -8,33 +8,31 @@ errors = ["ERROR","N E W ","WARNING","FATAL"]
 not_erros= ["ERROR_LOG","Error de comunicacion","Error executing command","npreFindClose","SendRecvTCPMsg resource","InsertResource resource","WARNING_LOG","COD0028","npreMsg2_SendRecvEx","GetResource:","Configuration parameter","ChangeItemPriceBySaleType","PosShowPrice","PosDoTryGrillEnd","cPosCheckPromotedOrder","PosConvertProduct","The Offers Server took more","sharpmessaging.cpp{DEP}@225","npreMemory.c","npDrvPublEVT.c"]
 
 Tk().withdraw()
+
 pregunta = input('Que accion queres tomar?: \n\t1- Resumir Log\n\t2- Busqueda de solo errores\n\t3- Salir\n')
 if pregunta == '3':exit()
-
 if pregunta == '1':
-    infile = askopenfilename()
-    outfile = asksaveasfilename()
-    with open(infile, "r") as inf, open(outfile, "w") as outf:
-        for line in inf:
-            if not any(phrase in line for phrase in not_log): 
-                if any(phrase in line for phrase in log):       
-                    f = line.split()  
-                    del f [0:5]
-                    delimited=' '
-                    b = delimited.join(f)          
-                    outf.write(b + '\n')
-
+            infile = askopenfilename()
+            outfile = asksaveasfilename()
+            with open(infile, "r") as inf, open(outfile, "w") as outf:
+                for line in inf:
+                    if not any(phrase in line for phrase in not_log): 
+                        if any(phrase in line for phrase in log):       
+                            f = line.split()  
+                            del f [0:5]
+                            delimited=' '
+                            b = delimited.join(f)          
+                            outf.write(b + '\n')
+    
 elif pregunta == '2':
-    infile = askopenfilename()
-    error = asksaveasfilename()
-    with open(infile, "r") as inf, open(error, "w")as errores:
-        for line in inf:
-            if any(phrase in line for phrase in errors):
-                    if not any(phrase in line for phrase in not_erros):  
-                        f = line.split()
-                        del f [0:5]
-                        delimited=' '
-                        b = delimited.join(f)
-                        errores.write(b + '\n')
-                
-
+            infile = askopenfilename()
+            error = asksaveasfilename()
+            with open(infile, "r") as inf, open(error, "w")as errores:
+                for line in inf:
+                    if any(phrase in line for phrase in errors):
+                            if not any(phrase in line for phrase in not_erros):  
+                                f = line.split()
+                                del f [0:5]
+                                delimited=' '
+                                b = delimited.join(f)
+                                errores.write(b + '\n')
