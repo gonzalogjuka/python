e495ebf6a745c6472c76e0dfafbf3f425bbff7d0
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-11 20:13:49 -0300

    all pages

--- Code/index.py
+++ Code/index.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def principal():
     return render_template('index.html')
 
+@app.route('/logs')
+def busqueda_log():
+    return render_template('logs.html')
+
+@app.route('/errores')
+def busqueda_errores():
+    return render_template('errores.html')
+
 if __name__ == '__main__':
     app.run(debug=True,port=5017)
