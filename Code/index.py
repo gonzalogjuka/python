9d958c6d23f1704de3fdb1711110c8bb1c33e0b6
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-12 19:56:54 -0300

    informacion_ok

--- Code/index.py
+++ Code/index.py
@@ -11,12 +11,14 @@ def principal():
 
 @app.route('/logs')
 def busqueda_log():
-        return render_template('logs.html') 
+            bases[0]
+            bases[1] # les pasamos las bases al template
+            return render_template('logs.html',base_0=bases[0],base_1=bases[1]) 
 
 
 @app.route('/errores')
 def busqueda_errores():
     return render_template('errores.html')
-
+     
 if __name__ == '__main__':
     app.run(debug=True,port=5017)
