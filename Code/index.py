3675fa652941cc68b72f484e443763ccaecae99c
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-12 15:57:04 -0300

    tutorial flask

--- Code/index.py
+++ Code/index.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template
+from ddbb import db
+from tkinter import Tk 
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+Tk().withdraw()
+bases = db()
 app=Flask(__name__)
 
 @app.route('/')
@@ -7,7 +12,21 @@ def principal():
     return render_template('index.html')
 
 @app.route('/logs')
+
 def busqueda_log():
+    bases[0]
+    bases[1]
+    infile = askopenfilename() # verificar si podemos pedir el file en el browser y no llamar a tkinter
+    outfile = asksaveasfilename(defaultextension='.html',initialfile = "resultado_log",filetypes=[('all files','*.*')])
+    with open(infile, "r") as inf, open(outfile, "w") as outf:
+                for line in inf:
+                    if not any(phrase in line for phrase in bases[0]): 
+                        if any(phrase in line for phrase in bases[1]):       
+                            f = line.split()  
+                            del f [0:5]
+                            delimited=' '
+                            b = delimited.join(f)       
+                            outf.write(b + '\n')   
     return render_template('logs.html')
 
 @app.route('/errores')
