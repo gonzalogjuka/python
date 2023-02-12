75a58738299e2f016fdcc52de26344a7646a57a6
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-12 19:48:15 -0300

    voides

--- Code/index.py
+++ Code/index.py
@@ -1,10 +1,9 @@
 from flask import Flask,render_template,request,redirect,url_for
 from ddbb import db
-import os
 
 bases = db()
 app=Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './Archivos_log'
+
 
 @app.route('/')
 def principal():
@@ -12,20 +11,11 @@ def principal():
 
 @app.route('/logs')
 def busqueda_log():
-    return render_template('logs.html') 
+        return render_template('logs.html') 
 
 
 @app.route('/errores')
 def busqueda_errores():
-    bases[0]
-    bases[1]
-    if request.method == 'POST':
-    # obtenemos el archivo del input "archivo"
-        f = request.files['archivo']  
-        filename = (f.filename)
-    # Guardamos el archivo en el directorio "Archivos_log"
-        f.save(os.path.join(app.config['UPLOAD_FOLDER']))
-    # Retornamos una respuesta satisfactoria
     return render_template('errores.html')
 
 if __name__ == '__main__':
