54d9a7a9533228ba06f27763accb81f29f98eb90
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-17 02:30:31 -0300

    almost_v.1.0

--- Code/CleanLog.py
+++ Code/CleanLog.py
@@ -49,6 +49,7 @@ def busqueda_logs():
             bases[0]
             bases[1]
             bases[4]
+            #x = list()
             infile = askopenfilename()
             outfile = asksaveasfilename(defaultextension='.html',initialfile = "resultado_log",filetypes=[('all files','*.*')])
             with open(infile, "r") as inf, open(outfile, "w") as outf:
@@ -59,11 +60,12 @@ def busqueda_logs():
                         if any(phrase in line for phrase in bases[1]):                            
                             f = line.split()
                             del f [0:5]
-                            for x in f:
-                                if any(pharse in x for pharse in bases[4]):
-                                    marcador = ('<mark>' + x + '</mark>')
-                                    f.append(marcador)
-                                    break                                                                                                                           
+                            for i in range(len(f)):
+                                    for palabra in f:                                                           
+                                        if any(pharse in palabra for pharse in bases[4]):
+                                            marcador = ('<mark>' + palabra + '</mark>')
+                                            f[i] = marcador.replace([i],marcador)
+                                            break                                                                                                                           
                             delimited=' '
                             b = delimited.join(f)         # pasa la informacion, solo reemplazar              
                             outf.write('<h5>' + b + '</h5>') 
