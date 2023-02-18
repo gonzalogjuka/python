f7b7d5097426b3e2dd77234931edb65565269ed5
Gonza <gonzalo.gjuka@hotmail.com>
2023-02-18 20:29:45 -0300

    add_all_pages

--- Code/index.py
+++ Code/index.py
@@ -11,7 +11,7 @@ def parseador_log():
             bases[1]
             bases[4]
             infile = askopenfilename()
-            outfile = asksaveasfilename(defaultextension='.html',initialfile = "contenido_xml",filetypes=[('all files','*.*')])
+            outfile = asksaveasfilename(defaultextension='.html',initialfile = "contenido_logs",filetypes=[('all files','*.*')])
             with open(infile, "r") as inf, open(outfile, "w") as outf:
                 outf.write('{% extends "base.html" %} {% block title %} Welcome Morpheus {% endblock %}')
                 outf.write('{% block body %}')
@@ -29,7 +29,29 @@ def parseador_log():
                             b = delimited.join(f)
                             outf.write('<h5>' + b + '</h5>')
                 outf.write('{% endblock %}')
-                            
+def parseador_errores():
+            bases[2]
+            bases[3]
+            bases[5]
+            infile = askopenfilename()
+            outfile = asksaveasfilename(defaultextension='.html',initialfile = "contenido_errores",filetypes=[('all files','*.*')])
+            with open(infile, "r") as inf, open(outfile, "w") as outf:
+                outf.write('{% extends "base.html" %} {% block title %} Welcome Morpheus {% endblock %}')
+                outf.write('{% block body %}')
+                for line in inf:
+                    if not any(phrase in line for phrase in bases[2]):
+                        if any(phrase in line for phrase in bases[3]):                  
+                            f = line.split()
+                            del f [0:5]
+                            for i, palabra in enumerate(f):                                                
+                                if any(pharse in palabra for pharse in bases[5]):
+                                    marcador = ('<mark>' + palabra + '</mark>')
+                                    f[i] = marcador
+                                    break                                                                                                          
+                            delimited=' '
+                            b = delimited.join(f)
+                            outf.write('<h5>' + b + '</h5>')
+                outf.write('{% endblock %}')                           
 
 @app.route('/')
 def principal():
@@ -42,16 +64,26 @@ def busqueda_log():
 @app.route('/resultado')
 def resultado():
     parseador_log()
-    return render_template('contenido_xml.html')
+    return render_template('contenido_logs.html')
 
-@app.route('/mostrar_resultado')
-def mostrar_resultado():
-    return render_template('contenido_xml.html')
+@app.route('/mostrar_log')
+def mostrar_log():
+    return render_template('contenido_logs.html')
+
 
 @app.route('/errores')
 def busqueda_errores():
     return render_template('errores.html')
 
+@app.route('/resultado_errores')
+def resultado_errores():
+    parseador_errores()
+    return render_template('contenido_errores.html')
+
+
+@app.route('/mostrar_errores')
+def mostrar_errores():
+    return render_template('contenido_errores.html')
      
 if __name__ == '__main__':
     app.run(debug=True,port=5017)
