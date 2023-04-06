58582a2bc2ac2c948e4765e61f714e0ae262c4a9
Gonza <gonzalo.gjuka@hotmail.com>
2023-04-06 19:53:30 -0300

    menu_ok

--- Code/lectura_tpa.py
+++ Code/lectura_tpa.py
@@ -1,22 +1,25 @@
-import tkinter
-import requests
+while True:
+    print('----- MENÚ -----')
+    print('1: Seleccionar número de pos de waystation')
+    print('2: Ingresar IP de pos a analizar')
+    print('0: Salir')
+    opcion = input('Ingrese una opción: ')
+    if opcion == '1':
+        pos_number = input('Ingrese número de pos: ')
+        url_way = r"D:\newpos61\POSFILES\LOGS\tlog\POS00" + pos_number
+        print('El número de pos es el siguiente: ' + url_way)
+    elif opcion == '2':
+        ip_pos = input('Ingrese IP completa de pos: ')
+        url_pos = "\\" + ip_pos + r"\e$\newpos61\posfile\logs"
+        print('La IP elegida es: ' + ip_pos)
+    elif opcion == '0':
+        print('Saliendo...')
+        break
+    else:
+        print('Opción no válida. Por favor, ingrese una opción válida.')
 
-opcion = input('Ingrese una opcion: \n1: Seleccionar numero de pos de waystation \n2: Ingresar ip de pos a analizar \n')
-
-# hacer menu, pedir pos , fecha y ip completa
-if 1 == opcion :
-    pos_number=input('Ingrese numero de pos: ')
-    url_way= r"D:\newpos61\POSFILES\LOGS\tlog\POS00"+pos_number
-    print('El numero de pos es el siguiente' + url_way)
-elif opcion == 2:
-    ip_pos=input('Ingrese ip completa de pos: ')
-    url_pos= rf"\\"+rf"{ip_pos}"+r"\e$\newpos61\posfile\logs"
-    print('La ip elegida es: ' + ip_pos)
 
 
 # hacer una boton de volver
 # una vez que toma la info correcta analizar tpa y anotar evt contarlos y mostrarlos (dar una preview de los eventos)
 # hacer seccion de resultados de los test (si tiene o no el resultado obtenido) y poner ok o fail
-
-# url predeterminada way D:\newpos61\POSFILES\LOGS\tlog\POS00
-# url predeterminada de las pos e$\newpos61\posfile\logs
