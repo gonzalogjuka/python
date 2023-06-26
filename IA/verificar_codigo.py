7ffffd7a8c0b48439d85490b23e58dcb89210a43
Gonza <gonzalo.gjuka@hotmail.com>
2023-06-25 21:29:42 -0300

    test_ok_module1_2

--- IA/verificar_codigo.py
+++ IA/verificar_codigo.py
@@ -20,25 +20,27 @@ class MinutaReporter(TextReporter):
     def handle_message(self, msg):
         super().handle_message(msg)
 
-        # Incrementar el contador de objetos llamativos
-        self.llamativos += 1
+        # Verificar si el mensaje es de categoría "error"
+        if msg.category == "error":
+            # Incrementar el contador de objetos llamativos
+            self.llamativos += 1
 
-        # Obtener las sugerencias asociadas al mensaje
-        suggestions = getattr(msg, 'suggestions', [])
+            # Obtener las sugerencias asociadas al mensaje
+            suggestions = getattr(msg, 'suggestions', [])
 
-        if suggestions:
-            # Agregar las sugerencias al informe
-            self.out.write(f"\nSugerencias para {msg.symbol}:\n")
-            for suggestion in suggestions:
-                self.out.write(f" - {suggestion}\n")
+            if suggestions:
+                # Agregar las sugerencias al informe
+                self.out.write(f"\nSugerencias para {msg.symbol}:\n")
+                for suggestion in suggestions:
+                    self.out.write(f" - {suggestion}\n")
 
-        # Agregar explicación detallada para cada error
-        self.out.write(f"\nExplicación para {msg.msg_id}:\n")
-        self.out.write(f" - Categoría: {msg.category}\n")
-        self.out.write(f" - Descripción: {msg.msg}\n")
-        self.out.write(f" - Archivo: {msg.path}\n")
-        self.out.write(f" - Línea: {msg.line}\n")
-        self.out.write(f" - Columna: {msg.column}\n")
+            # Agregar explicación detallada para cada error
+            self.out.write(f"\nExplicación para {msg.msg_id}:\n")
+            self.out.write(f" - Categoría: {msg.category}\n")
+            self.out.write(f" - Descripción: {msg.msg}\n")
+            self.out.write(f" - Archivo: {msg.path}\n")
+            self.out.write(f" - Línea: {msg.line}\n")
+            self.out.write(f" - Columna: {msg.column}\n")
 
     def display_messages(self):
         super().display_messages(None)
@@ -54,7 +56,7 @@ def verificar_codigo():
     linter.load_default_plugins()
     linter.check([archivo])
 
-    # Generar archivo de salida en formato de minuta con las verificaciones y sugerencias comentadas
+    # Generar archivo de salida en formato de minuta con los errores de categoría "error"
     with open("verificacion_minuta.txt", "w") as archivo_salida:
         reporter = MinutaReporter(archivo_salida)
         linter.set_reporter(reporter)
@@ -71,6 +73,3 @@ def verificar_codigo():
 
 # Llamar a la función principal
 verificar_codigo()
-
-
-# agregar solo los de tipo error que eviten la ejecucion y no todo lo demas
